16f3600c114f77d4f3854743573c3b7ebcee21b2
H.Sadri <hossein.sadri@outlook.com>
2019-09-06 04:49:40 +0430

    Brand_Product_Sadri_01

---
+++ OnlineMarketPlace/Models/AdminViewModels/ProductAbstractViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineMarketPlace.Models.AdminViewModels
+{
+    public class ProductAbstractViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("نام  محصول")]
+        public string Name { get; set; }
+
+    }
+}
---
+++ OnlineMarketPlace/Models/AdminViewModels/‌BrandViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineMarketPlace.Models.AdminViewModels
+{
+    public class BrandViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("نام برند ")]
+        public string Name { get; set; }
+        [DisplayName("توضیحات")]
+        public string Description { get; set; }
+
+    }
+}
